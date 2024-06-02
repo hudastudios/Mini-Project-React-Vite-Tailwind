505f1afb21a4b0e679ca6c21bef7ebf908be3794
hudastudios <studios.huda@gmail.com>
2024-06-02 23:10:09 +0700

    edit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Mini-Project-React-Vite-Tailwind/",
+  base: "/",
   plugins: [react()],
 })
